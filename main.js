b45af63c76b97cd7ba5d80348bf3918ac3edb4db
Khushi Sarkari <ksarkari@ex2india.com>
2022-08-23 11:55:05 +0530

    add success and failure modal and reset game

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = document.getElementById('app');
 
-const words = [];
+let words = [];
 let letters = [];
+
 const wordOfTheDay = 'rapid';
 const result = {
     guesses: 0,
@@ -23,6 +24,10 @@ const generateWordleBlocks = (rowId, wordLength) => {
 
 
 const renderWordlePanel = (wordLength, guesses) => {
+    const wordleWrapper = document.querySelector('.wordle-wrapper');
+    if (wordleWrapper) {
+        wordleWrapper.remove();
+    }
     const keyBoard = document.querySelector('.keyboard_wrapper');
     const container = document.createElement("div");
     container.classList.add('wordle-wrapper');
@@ -33,11 +38,36 @@ const renderWordlePanel = (wordLength, guesses) => {
     app.insertBefore(container, keyBoard);
 }
 
-renderWordlePanel(5, 6);
 const wordleRows = document.querySelectorAll('.wordle-wrapper .row');
 const keyboardKeys = document.querySelectorAll('.keyboard_wrapper .key .row span');
 const enterKey = document.querySelector('.keyboard_wrapper .key .row .enter-key');
 const delKey = document.querySelector('.keyboard_wrapper .key .row .del-key');
+const closeModal = document.querySelector('.modal__close');
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal__content');
+
+
+const showSuccessModal = () => {
+    modal.style.visibility = 'visible';
+    modal.style.opacity = 1;
+    modalContent.querySelector('h1').textContent = 'Great Job!'
+    modalContent.querySelector('p').textContent = 'You have cracked the game!'
+}
+
+const showFailureModal = () => {
+    const modal = document.querySelector('.modal');
+    modal.style.visibility = 'visible';
+    modal.style.opacity = 1;
+    modalContent.querySelector('h1').textContent = 'OOPS!'
+    modalContent.querySelector('p').textContent = 'Better luck next time!'
+}
+
+const resetGame = () => {
+    words = [];
+    letters = [];
+    renderWordlePanel(5, 6);
+
+}
 
 const paintDataToBlocks = (e) => {
     const data = e.target.dataset.key;
@@ -52,10 +82,13 @@ const paintDataToBlocks = (e) => {
 }
 
 const onEnter = () => {
+    result.guesses++;
+    let correctLetters = 0;
     letters.forEach((letter, idx) => {
         const block = document.querySelector(`.row-${words.length} .block-${idx + 1}`);
         if (wordOfTheDay.includes(letter) && wordOfTheDay.indexOf(letter) === idx) {
             block.classList.add('match-found');
+            correctLetters++;
         } else if (wordOfTheDay.includes(letter) && wordOfTheDay.indexOf(letter) !== idx) {
             block.classList.add('letter-present');
         } else {
@@ -64,6 +97,13 @@ const onEnter = () => {
         block.classList.add('traversed');
     });
     letters = [];
+    if (result.guesses === 6) {
+        result.outcome = 'failed';
+        showFailureModal();
+    } else if (correctLetters === 5) {
+        result.outcome = 'success'
+        showSuccessModal();
+    }
 };
 const onDelete = () => {
     const block = document.querySelector(`.row-${words.length + 1} .block-${letters.length}`);
@@ -72,13 +112,23 @@ const onDelete = () => {
 }
 
 keyboardKeys.forEach((key) => {
-    key.addEventListener('click', (e) => {
-        if (e.target.classList.contains('enter-key')) {
-            onEnter();
-        } else if (e.target.classList.contains('del-key')) {
-            onDelete();
-        } else {
-            paintDataToBlocks(e);
-        }
-    });
+    if (!result.outcome) {
+        key.addEventListener('click', (e) => {
+            if (e.target.classList.contains('enter-key')) {
+                onEnter();
+            } else if (e.target.classList.contains('del-key')) {
+                onDelete();
+            } else {
+                paintDataToBlocks(e);
+            }
+        });
+    }
 });
+
+closeModal.addEventListener('click', (e) => {
+    resetGame();
+    modal.style.visibility = 'hidden';
+    modal.style.opacity = 0;
+})
+
+renderWordlePanel(5, 6);
